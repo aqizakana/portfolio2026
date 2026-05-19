e682fe8fb4d217031500087b8a400635c648c877
UoTA <130530568+aqizakana@users.noreply.github.com>
2026-05-19 22:16:44 +0900

    Revert "feat: nitro"

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import glsl from "vite-plugin-glsl";
-import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   plugins: [
@@ -20,6 +19,5 @@ export default defineConfig({
       },
     }),
     viteReact(),
-    nitro(),
   ],
 });
